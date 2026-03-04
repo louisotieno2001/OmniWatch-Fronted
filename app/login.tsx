e9de0166bb71973f2bba20b11f7d5b9c333fe876
louisotieno2001 <louisotieno2001@gmail.com>
2026-03-04 16:19:34 +0300

    fix: fixed unresolvable coma with hex code values

--- app/login.tsx
+++ app/login.tsx
@@ -209,7 +209,7 @@ export default function LoginScreen() {
 
         {/* Footer */}
         <View style={styles.footer}>
-          <Text style={styles.footerText}>Don't have an account?</Text>
+          <Text style={styles.footerText}>Don&apos;t have an account?</Text>
           <TouchableOpacity>
             <Text style={styles.link} onPress={() => router.push('/signup')}> Sign Up</Text>
           </TouchableOpacity>
